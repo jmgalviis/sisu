7c5772843556fd63d4524aeb682b4eb55a8da00c
jmgalvis <jmgalviis@gmail.com>
2020-05-22 12:46:47 -0500

    Se agregaron los archivos de estilo y javascript para el diseño de interfaces

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,10 +14,17 @@
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,700&amp;subset=latin-ext"
+          rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/fonts/batch-icons/css/batch-icons.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/bootstrap/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/bootstrap/mdb.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/plugins/custom-scrollbar/jquery.mCustomScrollbar.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/hamburgers/hamburgers.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/fonts/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/quillpro/quillpro.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
@@ -76,5 +83,15 @@
             @yield('content')
         </main>
     </div>
+    <script src="{{ asset('js/jquery/jquery-3.1.1.min.js') }}" defer></script>
+    <script src="{{ asset('js/bootstrap/popper.min.js') }}" defer></script>
+    <script src="{{ asset('js/bootstrap/bootstrap.min.js') }}" defer></script>
+    <script src="{{ asset('js/bootstrap/mdb.min.js') }}" defer></script>
+    <script src="{{ asset('js/plugins/velocity/velocity.min.js') }}" defer></script>
+    <script src="{{ asset('js/plugins/velocity/velocity.ui.min.js') }}" defer></script>
+    <script src="{{ asset('js/plugins/velocity/velocity.ui.min.js') }}" defer></script>
+    <script src="{{ asset('js/plugins/custom-scrollbar/jquery.mCustomScrollbar.concat.min.js') }}" defer></script>
+    <script src="{{ asset('js/plugins/jquery_visible/jquery.visible.min.js') }}" defer></script>
+    <script src="{{ asset('js/misc/ie10-viewport-bug-workaround.js') }}" defer></script>
 </body>
 </html>
